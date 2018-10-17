7d656ce7afef387d5e0d8fd6cc5b39ec071a0156
„Waldek” <reclikmail@gmail.com>
2018-10-17 12:11:29 +0200

    ADMIN | personalizacja panelu admina w pliku admin.py dla modelu klasy Movie

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+
+# inna fajna forma rejestrowania modelu w panelu admina
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    """ fields - pola które mają być wyświetlane na stronie edycji filmu
+    fields = ('name', 'description') """
+
+    list_display = ('name', 'year', 'imdb_rating', 'relaesed')
+    list_filter = ('year', 'imdb_rating')   # filtr boczny
+    search_fields = ('name', 'description')
+    ordering = ('-imdb_rating', '-year')
