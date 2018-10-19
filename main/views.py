55651b5e8e2a74290d0a2526e0d517797a11c77a
„Waldek” <reclikmail@gmail.com>
2018-10-19 10:18:32 +0200

    CRUD (READ) | Zrobiłem stronę wyświetlającą wszystkie filmy na szablonie z widoku 'wszystkie_filmy' i i jego templatce 'lista_filmow'. Zmieniłem zmienną w widoku na dynamiczne pobieranie danych (QuerySet)

--- main/views.py
+++ main/views.py
@@ -1,10 +1,10 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 
 def wszystkie_filmy(request):      # każda funkcja w views przyjmuje parametr request
     # return HttpResponse('testuję bo umiem')
-    text11 = 'w ten sposób mogę przekazać zmienną pochodzącą z funkcji | wynik dodawania to: ' + str(44 + 11)
-    zmienna_filmy = ['Avatar', '12 angry people', 'MadMax', 'MadMax2', 'MadMax3']
-    return render(request, 'lista_filmow.html', {'text44': text11, 'filmowa': zmienna_filmy, 'moze_ogladac': True})
-
+    zmienna44 = "mięta" * 11
+    zmienna_filmy = Movie.objects.all()
+    return render(request, 'lista_filmow.html', {'filmowa': zmienna_filmy, 'text44': zmienna44})
