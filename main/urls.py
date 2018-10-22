e474f20c455eff23aed2e4490f7ad1d04ddf6963
„Waldek” <reclikmail@gmail.com>
2018-10-22 12:06:06 +0200

    User Interface | tworzę proste UI dla CRUDa za pomocą href htmla, żeby użytkownik miał dostęp do wszystkich opcji CRUD

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views import wszystkie_filmy, nowy_film, edytuj_film, usun_film
-
+"""
+do path dodaję atrybut 'name' aby potem w templatkach
+móc się do nich odwoływać, przykład: {% url 'name' film.id %}
+-------------------------------
+<int:id> pobiera numer id filmu który chcę edytować
+"""
 
 urlpatterns = [
-    path('wszystkie/', wszystkie_filmy),     # jeśli url localhost/main/wszystkie to do funkcji wszystkie_filmy w views
-    path('nowy/', nowy_film),
-    path('edytuj/<int:id>/', edytuj_film),   # <int:id> pobiera numer id filmu który chcę edytować
-    path('usun/<int:id>/', usun_film)      # <int:id> pobiera numer id filmu który chcę edytować
+    path('wszystkie/', wszystkie_filmy, name='wszystkie_filmy'),     # jeśli url localhost/main/wszystkie to do funkcji wszystkie_filmy w views
+    path('nowy/', nowy_film, name='nowy_film'),
+    path('edytuj/<int:id>/', edytuj_film, name='edytuj_film'),
+    path('usun/<int:id>/', usun_film, name='usun_film')
 ]
