ced7d3305ac68c9d58c9f9d79ac195b4cce1f750
„Waldek” <reclikmail@gmail.com>
2018-10-22 08:53:53 +0200

    CRUD (UPDATE) | url do strony 'edytuj/id filmu' | tworzę widok edytuj_film podobnie jak w CREATE tylko przypisując do formularza 'instance' który pobiera id filmu | dodatkowo zmieniłem nazwę 'nowy_film' na 'film_form' w widokach i templatce

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import wszystkie_filmy, nowy_film
+from .views import wszystkie_filmy, nowy_film, edytuj_film
 
 
 urlpatterns = [
     path('wszystkie/', wszystkie_filmy),    # jeśli url localhost/main/wszystkie to do funkcji wszystkie_filmy w views
-    path('nowy/', nowy_film)
+    path('nowy/', nowy_film),
+    path('edytuj/<int:id>/', edytuj_film)   # <int:id> pobiera numer id filmu który chcę edytować
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 # from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Movie
 from .forms import MovieForm
 
@@ -10,6 +10,7 @@ def wszystkie_filmy(request):      # każda funkcja w views przyjmuje parametr r
     zmienna_filmy = Movie.objects.all()
     return render(request, 'lista_filmow.html', {'filmowa': zmienna_filmy, 'text44': zmienna44})
 
+
 def nowy_film(request):
     form = MovieForm(request.POST or None, request.FILES or None)
 
@@ -17,4 +18,15 @@ def nowy_film(request):
         form.save()
         return redirect(wszystkie_filmy)
 
-    return render(request, 'nowy_film.html', {'form': form})
+    return render(request, 'film_form.html', {'form': form})
+
+
+def edytuj_film(request, id):
+    film = get_object_or_404(Movie, pk=id)
+    form = MovieForm(request.POST or None, request.FILES or None, instance=film)
+
+    if form.is_valid():
+        form.save()
+        return redirect(wszystkie_filmy)
+
+    return render(request, 'film_form.html', {'form': form})
