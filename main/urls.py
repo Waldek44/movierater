2ceeff74faa1b022a9ab6389a45f0669c7a22b55
„Waldek” <reclikmail@gmail.com>
2018-10-22 09:33:47 +0200

    CRUD (DELETE) | url do strony 'usun/id filmu' | tworzę widok i templatkę

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import wszystkie_filmy, nowy_film, edytuj_film
+from .views import wszystkie_filmy, nowy_film, edytuj_film, usun_film
 
 
 urlpatterns = [
-    path('wszystkie/', wszystkie_filmy),    # jeśli url localhost/main/wszystkie to do funkcji wszystkie_filmy w views
+    path('wszystkie/', wszystkie_filmy),     # jeśli url localhost/main/wszystkie to do funkcji wszystkie_filmy w views
     path('nowy/', nowy_film),
-    path('edytuj/<int:id>/', edytuj_film)   # <int:id> pobiera numer id filmu który chcę edytować
+    path('edytuj/<int:id>/', edytuj_film),   # <int:id> pobiera numer id filmu który chcę edytować
+    path('usun/<int:id>/', usun_film)      # <int:id> pobiera numer id filmu który chcę edytować
 ]
--- main/views.py
+++ main/views.py
@@ -30,3 +30,19 @@ def edytuj_film(request, id):
         return redirect(wszystkie_filmy)
 
     return render(request, 'film_form.html', {'form': form})
+
+"""
+usuń film działa w ten sposób:
+po pierwsze wyświetli nam potwierdz.html (ponieważ if == False)
+żeby if się wykonał musi dostać POST z formularza potwierdz.html 
+(z przekazaną zmienną 'film' - wtedy wie który film usunąć)
+jeśli otrzymałeś request typu POST to wydarza się if i skasuje film
+"""
+def usun_film(request, id):
+    film = get_object_or_404(Movie, pk=id)
+
+    if request.method == 'POST':
+        film.delete()
+        return redirect(wszystkie_filmy)
+
+    return render(request, 'potwierdz.html', {'film': film})
